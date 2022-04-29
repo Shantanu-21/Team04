ab7e8c3a65c68d9d1db8910d54d2ff7a709b4c43
Sagar4971 <sagarnerkar2912@gmail.com>
2022-04-29 13:08:12 +0530

    sagar Nerkar

---
+++ src/main/java/com/Team04/sagar/Addition.java
@@ -0,0 +1,17 @@
+package com.Team04.sagar;
+
+public class Addition {
+	
+	
+	public static void main(String[] args) {
+		int a=12;
+		int b=20;
+		int c;
+		
+		c=a+b;
+		System.out.println(c);
+		
+		
+	}
+
+}
