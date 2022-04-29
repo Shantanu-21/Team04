ee59e771e038548de1498eca4cc318352e8a2127
Shantanu-21 <shantanumogre21@gmail.com>
2022-04-29 13:19:57 +0530

    shantanu mogre

---
+++ src/main/java/com/Team04/shantanu/shantanu.java
@@ -0,0 +1,20 @@
+package com.Team04.shantanu;
+
+public class shantanu {
+	
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		
+		System.out.println("before a and b "+a+" "+b);
+		
+		int c=a;
+		a=b;
+		b=c;
+		
+		System.out.println("before a and b "+a+" "+b);
+		
+	}
+
+}
