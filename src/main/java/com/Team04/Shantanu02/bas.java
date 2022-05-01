5b0c60a2bb774bb6b9bf33e7711757aa0c3b866b
Shantanu-21 <shantanumogre21@gmail.com>
2022-05-01 12:40:52 +0530

    baki

---
+++ src/main/java/com/Team04/Shantanu02/bas.java
@@ -0,0 +1,15 @@
+package com.Team04.Shantanu02;
+
+public class bas {
+
+	public static void main(String[] args) {
+		
+		System.out.println("git stash ");
+		
+		System.out.println("done");
+		
+		System.out.println("nikki");
+		
+	}
+	
+}
