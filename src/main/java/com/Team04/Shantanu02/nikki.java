a83b7904a7f3d53c0f9b6dc17d5a956dc633709a
Shantanu-21 <shantanumogre21@gmail.com>
2022-05-01 12:36:04 +0530

    sunday

---
+++ src/main/java/com/Team04/Shantanu02/nikki.java
@@ -0,0 +1,9 @@
+package com.Team04.Shantanu02;
+
+public class nikki {
+	
+	public static void main(String[] args) {
+		 System.out.println("nikki");
+	}
+
+}
---
+++ src/main/java/com/Team04/Shantanu02/sunday.java
@@ -0,0 +1,11 @@
+package com.Team04.Shantanu02;
+
+public class sunday {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("sunday");
+	}
+
+}
